2dd4d1725d4990a73efc6ab1965bcc5260814766
Dominik Meurer <50107525+DMeurer@users.noreply.github.com>
2020-11-29 15:29:16 +0100

    installed discord.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+
+const client = new Discord.client();
+
+/*
+client.once('ready'), () => {
+  console.log('Domi-Dev Bot is Online!')
+};
+
+*/
+
+client.login('NzgyNjAxNjk3NDYwODc5Mzkx.X8OkoA.-yotqg8w01wAzXhw_6-5D81pCUg')
