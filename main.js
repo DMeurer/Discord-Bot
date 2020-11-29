8f5ee374604dc90f8f2196c7aca44ca44230f0c6
Dominik Meurer <50107525+DMeurer@users.noreply.github.com>
2020-11-29 16:14:50 +0100

    made a bot

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
 const Discord = require('discord.js')
 
-const client = new Discord.client();
+const fs = require('fs')
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 
-/*
-client.once('ready'), () => {
-  console.log('Domi-Dev Bot is Online!')
-};
+var client = new Discord.Client()
 
-*/
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}`)
+})
 
-client.login('NzgyNjAxNjk3NDYwODc5Mzkx.X8OkoA.-yotqg8w01wAzXhw_6-5D81pCUg')
+//Init finish
+
+var cmdmap = {
+  say: cmd_say,
+  test: cmd_test
+}
+
+function cmd_say(msg, args){
+  msg.channel.send(args.join(' '))
+}
+
+function cmd_test(msg, args){
+  console.log('test')
+}
+
+
+client.on('message', (msg) => {
+  var cont = msg.content,
+    author = msg.member,
+    channel = msg.channel,
+    guild = msg.guild
+  
+  if(author.is != client.user.id && cont.startsWith(config.prefix)){
+    var invoke = cont.split(' ')[0].substr(config.prefix.length),
+      args = cont.split(' ').slice(1)
+
+    console.log(invoke, args)
+
+    if(invoke in cmdmap){
+      cmdmap[invoke](msg, args)
+    }
+  }
+})
+
+client.login(config.token)
