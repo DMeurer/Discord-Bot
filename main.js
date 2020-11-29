226e582e01ecc403d8a37196b8f439a70ef479fe
Dominik Meurer <50107525+DMeurer@users.noreply.github.com>
2020-11-29 16:33:55 +0100

    just some commenting

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Some crazy importing stuff over here
+
 const Discord = require('discord.js')
 
 const fs = require('fs')
@@ -5,11 +7,16 @@ const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 
 var client = new Discord.Client()
 
+//Init finish
+
+//am I online?
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}`)
 })
 
-//Init finish
+//nailed it
+
 //cmdmap Begin
 
 var cmdmap = {
@@ -32,6 +39,9 @@ function cmd_ping(msg, args){
   msg.channel.send('pong')
 }
 
+//Botcommands set. Lets go!
+
+//He reads the messages to filter commands for the bot
 
 client.on('message', (msg) => {
   var cont = msg.content,
@@ -39,16 +49,24 @@ client.on('message', (msg) => {
     channel = msg.channel,
     guild = msg.guild
   
+  //filter messages. Look on the prefix and dont reply to onw messages
   if(author.is != client.user.id && cont.startsWith(config.prefix)){
+    //splitting the message in invoke and args without the prefix
     var invoke = cont.split(' ')[0].substr(config.prefix.length),
       args = cont.split(' ').slice(1)
 
+    //console output of the invoke and the args
     console.log(invoke, args)
 
+    //giving output:
     if(invoke in cmdmap){
       cmdmap[invoke](msg, args)
+    } else {
+      msg.channel.send('This is not a command')
     }
   }
 })
+
+//Logging in the Bot to change the world. Or at least the discord server.
 
 client.login(config.token)
